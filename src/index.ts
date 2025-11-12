b70698f9a9cf534418df9454bffad43ce46e5cda
Fabio Cicotti <cicotti@users.noreply.github.com>
2025-11-12 09:25:53 -0300

    Vercel: Fastfy log

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-import Fastify from 'fastify'
+import 'dotenv/config';
+import Fastify from 'fastify';
+//import cors from '@fastify/cors';
+import multipart from '@fastify/multipart';
+
+// Routes
+import authRoutes from './routes/auth.routes';
+import companiesRoutes from './routes/companies.routes';
+import clientsRoutes from './routes/clients.routes';
+import usersRoutes from './routes/users.routes';
+import projectsRoutes from './routes/projects.routes';
+import weatherRoutes from './routes/weather.routes';
+import photosRoutes from './routes/photos.routes';
+
+const PORT = parseInt(process.env.PORT || '3000', 10);
  
-const fastify = Fastify({ logger: true });
- 
-fastify.get('/', async (request, reply) => {
-  return { hi: 'What are you looking for?' }
+const fastify = Fastify({
+  logger: { level: process.env.LOG_LEVEL || 'info' }
 });
  
-fastify.listen({ port: 3000 });
+fastify.get('/', async (request, reply) => {
+  return { hi: 'Fastfy log' }
+});
+ 
+fastify.listen({ port: PORT });
